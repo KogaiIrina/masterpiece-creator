4376d3fdddfd4b89fbb308479ddd5d6edee51916
Irina Kogay <kogayie1995@gmail.com>
2022-10-23 12:37:30 +0400

    change the model's parametrs

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 import gradio as gr
 import lightning as L
+from lightning.app.storage import Drive
 from lightning.app.components.serve import ServeGradio
 
 class MasterpieceCreator(ServeGradio):
-    inputs = gr.inputs.Textbox(label="print your prompt here")
+    inputs = [gr.inputs.Textbox(label="print your prompt here"), gr.inputs.Number(default=250, label="number of steps")]
     outputs = gr.outputs.Image(type="auto", label="Your masterpiece is ready")
     enable_queue = True
 
-    def predict(self, prompt):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.drive_1 = Drive("lit://drive_1")
+
+    def predict(self, prompt, number_of_steps):
         results = self.model.create(
           text_prompts=prompt,
-          width_height=[512, 512],
+          width_height=[512, 448],
           n_batches=1,
+          steps=number_of_steps,
+          diffusion_model="watercolordiffusion",
+          clip_models=["ViT-B-32::openai"],
+          clip_guidance_scale=40000,
         )
+
+        file_name = f"./img2.png"
         result = results[0]
         result.load_uri_to_image_tensor()
+        result.save_image_tensor_to_file(file_name)
+        self.drive_1.put(file_name)
         return result.tensor
 
     def build_model(self):
